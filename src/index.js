9aaa25aacbac9b896c46fe46692eccb575c9983d
DayDream <kyhong222@naver.com>
2020-08-04 20:19:56 +0900

    convert window ver

--- src/index.js
+++ src/index.js
@@ -8,24 +8,33 @@ const client = new Discord.Client();
 
 const targetCronTable = "* * * * *";    // every min
 
-let User;
 let job;
+
+const message = "하이";
+function timeMessage(){
+  return `지금 시각은 ${Date.now()} 입니다.`
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if(msg.content.startsWith('준비')){
-    User = msg.author;
+  
+  if(msg.content === "인사"){
+    msg.author.send(message);
+  }
+  else if(msg.content.startsWith('시간')){
     job = schedule.scheduleJob(targetCronTable, function(){
-      User.send("test");
+      msg.author.send(`지금 시각은 ${timeMessage()} 입니다.`);
     });
   }
   else if(msg.content.startsWith('그만')){
     job.cancel();
   }
+  else if(msg.content === '테스트'){
+    msg.author.send('테스트');
+  }
 });
 
-
-client.login(config.token)
+client.login(process.env.token)
